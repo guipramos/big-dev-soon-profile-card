b72b940b29e104a09a08641e178c865478a9f01d
Guilherme Ramos <gui.naris.gr@gmail.com>
2024-09-29 21:49:34 -0300

    Adicionando arquivos do projeto finalizado

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <div className="content">
+      <Card />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,50 @@
+import bgheader from "../../assets/bg-header.png";
+import { FaFacebook, FaLinkedin } from "react-icons/fa";
+import "./styles.scss";
+
+const Card = () => {
+  return (
+    <article className="card">
+      <div className="header-img">
+        <img src={bgheader} alt="" />
+      </div>
+      <header>
+        <figure>
+          <img
+            src="https://www.github.com/guipramos.png"
+            alt="Header bg main"
+          />
+          <figcaption>
+            <h1>Johnny Rogers</h1>
+            <p>@jonnyrogers</p>
+          </figcaption>
+        </figure>
+      </header>
+      <section className="card-icon">
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/guihpramos" target="_blank">
+              <FaLinkedin />
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section className="card-content">
+        <p>
+          Crafting brand and communication strategies, creating visual designs,
+          leading art direction, and capturing portraits through photography.
+        </p>
+      </section>
+      <section className="card-btn">
+        <a href="#" className="card-btn-full">
+          Fallow
+        </a>
+        <a href="#" className="card-btn-outline">
+          Message
+        </a>
+      </section>
+    </article>
+  );
+};
+
+export default Card;
